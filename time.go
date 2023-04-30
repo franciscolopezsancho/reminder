7aac74aea466d1122ca8057a7006b16ecf34e523
Francisco Lopez-Sancho <franciscolopezsancho@gmail.com>
2023-04-30 19:05:26 +0200

    first commit

---
+++ time.go
@@ -0,0 +1,25 @@
+package memoryreminder
+
+import (
+	"fmt"
+	"math"
+	"time"
+
+	"google.golang.org/api/calendar/v3"
+)
+
+func dates(t time.Time) []calendar.EventDateTime {
+	var events []calendar.EventDateTime
+
+	for i := 0; i < 12; i++ {
+		numberDays := int(math.Pow(2, float64(i)))
+		events = append(events, calendar.EventDateTime{
+			Date: toString(t.Add(time.Duration(int(time.Hour) * 24 * numberDays))),
+		})
+	}
+	return events
+}
+
+func toString(t time.Time) string {
+	return fmt.Sprintf("%d-%d-%d", t.Year(), t.Month(), t.Day())
+}
